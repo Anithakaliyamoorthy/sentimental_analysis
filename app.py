6a6f912940c400b3d798e37f5c8a182ac3a03486
Anithakaliyamoorthy <146464884+Anithakaliyamoorthy@users.noreply.github.com>
2025-05-17 21:42:35 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from transformers import RobertaTokenizer, RobertaForSequenceClassification, pipeline
-from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 # Load model and tokenizer
